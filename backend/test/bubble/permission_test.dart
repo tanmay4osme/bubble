0f592ca76a9a82295f76595dce0df5e649914e48
Tobe O <thosakwe@gmail.com>
2019-04-23 13:25:05 -0400

    Use mirrors reflector in tests

--- backend/test/bubble/permission_test.dart
+++ backend/test/bubble/permission_test.dart
@@ -1,5 +1,6 @@
 import 'package:angel_orm/angel_orm.dart';
 import 'package:angel_test/angel_test.dart';
+import 'package:backend/src/routes/controllers/bubble.dart';
 import 'package:bubble/bubble.dart';
 import 'package:test/test.dart';
 import '../common.dart';
@@ -7,12 +8,14 @@ import '../common.dart';
 main() async {
   TestClient client;
   QueryExecutor executor;
+  BubbleController bubbleController;
 
   Bubble socksBubble;
 
   setUp(() async {
     client = await connectToApp();
     executor = client.server.container.make<QueryExecutor>();
+    bubbleController = client.server.container.make<BubbleController>();
 
     var socksBubbleQuery = BubbleQuery();
     socksBubbleQuery.values
@@ -26,5 +29,6 @@ main() async {
 
   test('hey', () async {
     print(socksBubble);
+    print(bubbleController.routeMappings);
   });
 }
--- backend/test/common.dart
+++ backend/test/common.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:angel_container/mirrors.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_migration_runner/angel_migration_runner.dart';
 import 'package:angel_test/angel_test.dart';
@@ -12,7 +13,8 @@ const testingEnv = AngelEnvironment('testing');
 Future<TestClient> connectToApp() async {
   hierarchicalLoggingEnabled = true;
   var logger = Logger.detached('bubble_test')..onRecord.listen(prettyLog);
-  var app = Angel(logger: logger, environment: testingEnv);
+  var app = Angel(
+      logger: logger, environment: testingEnv, reflector: MirrorsReflector());
   var runner = await createMigrationRunner(testingEnv);
   await runMigrations(runner, ['refresh']);
   await backend.configureServer(app, testingEnv);
