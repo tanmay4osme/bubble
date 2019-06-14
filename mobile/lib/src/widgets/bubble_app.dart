84a025722bb1ffa35b2db939f03e2030bfdc9876
Tobe O <thosakwe@gmail.com>
2019-06-14 01:27:27 -0400

    basic home

--- mobile/lib/src/widgets/bubble_app.dart
+++ mobile/lib/src/widgets/bubble_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bubble_home.dart';
 import 'gray_input.dart';
 
 class BubbleApp extends StatefulWidget {
@@ -14,83 +15,84 @@ class _BubbleAppState extends State<BubbleApp> {
         primaryColor: Color(0xff00d2b8),
       ),
       title: 'Bubble',
-      home: Builder(builder: (context) {
-        return Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16).copyWith(top: 96),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Image.asset(
-                      'assets/logo.png',
-                      height: 64,
-                    ),
-                    Text(
-                      'A new kind of network.',
-                      style: Theme.of(context)
-                          .textTheme
-                          .title
-                          .copyWith(color: Theme.of(context).primaryColor),
-                    ),
-                    // Text(
-                    // 'A new kind of network.',
-                    // style: Theme.of(context)
-                    //     .textTheme
-                    //     .subhead
-                    //     // .copyWith(color: Theme.of(context).primaryColor),
-                    //     .copyWith(color: Colors.grey[700]),
-                    // ),
-                    Divider(
-                      color: Colors.transparent,
-                      height: 32,
-                    ),
-                    GrayInput(
-                      placeholder: 'Username',
-                    ),
-                    Divider(color: Colors.transparent),
-                    GrayInput(
-                      placeholder: 'Password',
-                      obscureText: true,
-                    ),
-                    Divider(
-                      color: Colors.transparent,
-                      height: 32,
-                    ),
-                    Text(
-                      'Create an account...',
-                      style: Theme.of(context)
-                          .textTheme
-                          .subhead
-                          .copyWith(color: Theme.of(context).primaryColor),
-                    ),
-                  ],
-                ),
-              ),
-              Spacer(),
-              // alignment: Alignment.bottomCenter,
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  color: Theme.of(context).primaryColor,
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'SIGN IN »',
-                    textScaleFactor: 1.2,
-                    style: TextStyle(
-                      color: Colors.white,
+      home: BubbleHome() ??
+          Builder(builder: (context) {
+            return Scaffold(
+              backgroundColor: Colors.white,
+              body: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16).copyWith(top: 96),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Image.asset(
+                          'assets/logo.png',
+                          height: 64,
+                        ),
+                        Text(
+                          'A new kind of network.',
+                          style: Theme.of(context)
+                              .textTheme
+                              .title
+                              .copyWith(color: Theme.of(context).primaryColor),
+                        ),
+                        // Text(
+                        // 'A new kind of network.',
+                        // style: Theme.of(context)
+                        //     .textTheme
+                        //     .subhead
+                        //     // .copyWith(color: Theme.of(context).primaryColor),
+                        //     .copyWith(color: Colors.grey[700]),
+                        // ),
+                        Divider(
+                          color: Colors.transparent,
+                          height: 32,
+                        ),
+                        GrayInput(
+                          placeholder: 'Username',
+                        ),
+                        Divider(color: Colors.transparent),
+                        GrayInput(
+                          placeholder: 'Password',
+                          obscureText: true,
+                        ),
+                        Divider(
+                          color: Colors.transparent,
+                          height: 32,
+                        ),
+                        Text(
+                          'Create an account...',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subhead
+                              .copyWith(color: Theme.of(context).primaryColor),
+                        ),
+                      ],
                     ),
                   ),
-                  onPressed: () => null,
-                ),
+                  Spacer(),
+                  // alignment: Alignment.bottomCenter,
+                  SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      color: Theme.of(context).primaryColor,
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'SIGN IN »',
+                        textScaleFactor: 1.2,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () => null,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        );
-      }),
+            );
+          }),
     );
   }
 }
