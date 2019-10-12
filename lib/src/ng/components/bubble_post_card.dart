2ea130639b8ccce72490e845d1128aa46ce65deb
Tobe O <thosakwe@gmail.com>
2019-10-11 22:57:46 -0400

    Basic feed skeleton

---
+++ lib/src/ng/components/bubble_post_card.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'bubble-post-card',
+    templateUrl: 'bubble_post_card.html',
+    styleUrls: ['package:bubble/src/sass/post_card.css'])
+class BubblePostCardComponent {}
--- lib/src/ng/components/bubble_timeline.dart
+++ lib/src/ng/components/bubble_timeline.dart
@@ -1,4 +1,9 @@
 import 'package:angular/angular.dart';
+import 'bubble_post_card.dart';
 
-@Component(selector: 'bubble-timeline', templateUrl: 'bubble_timeline.html')
+@Component(
+    selector: 'bubble-timeline',
+    templateUrl: 'bubble_timeline.html',
+    directives: [BubblePostCardComponent],
+    styleUrls: ['package:bubble/src/sass/timeline.css'])
 class BubbleTimelineComponent {}
